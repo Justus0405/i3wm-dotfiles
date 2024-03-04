d44d231d1a0b95b22b376ae2dbb00107c5e77d7f
Justus0405 <143542687+Justus0405@users.noreply.github.com>
2024-03-04 17:00:24 +0100

    Add files via upload

---
+++ src/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export DIRMAIN=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
+
+echo Version 1.0
+
+sudo pacman -S pipewire lib32-pipewire pipewire-pulse pipewire-jack wireplumber rtkit i3-wm sddm polybar rofi alacritty nitrogen xf86-input-evdev pcmanfm-gtk3 lxappearance-gtk3 neofetch flameshot unzip qt5-graphicaleffects qt5-svg qt5-quickcontrols2 wget code discord steam 
+
+sudo usermod -a -G rtkit $USER
+systemctl --user enable pipewire pipewire-pulse
+
+sudo localectl set-keymap de
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -P $DIRMAIN/assets
+wget https://github.com/catppuccin/gtk/releases/download/v0.7.1/Catppuccin-Mocha-Standard-Mauve-Dark.zip -P $DIRMAIN/assets
+
+mkdir ~/.config ~/.fonts ~/.themes
+mkdir ~/.themes Catppuccin-Mocha-Standard-Mauve-Dark
+unzip $DIRMAIN/assets/Catppuccin-Mocha-Standard-Mauve-Dark.zip -d ~/.themes/Catppuccin-Mocha-Standard-Mauve-Dark
+unzip $DIRMAIN/assets/JetBrainsMono.zip -d ~/.fonts
+
+cp -r $DIRMAIN/config ~/.config
+
+sudo cp -r $DIRMAIN/assets/sddm/catppuccin-mocha /usr/share/sddm/themes/
+sudo cp -r $DIRMAIN/assets/sddm.conf /etc/
+
+sudo mv /usr/share/X11/xorg.conf.d/40-libinput.conf ~/.config
+sudo cp -r $DIRMAIN/assets/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
+
+nitrogen --set-zoom-fill .config/background.jpg
+
+sudo systemctl enable sddm
+
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo yay -S brave-bin
+
+reboot
