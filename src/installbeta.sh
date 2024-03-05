fd58e2dbc2d9df01ebc507573e17fb0e2cb5642f
Justus0405 <143542687+Justus0405@users.noreply.github.com>
2024-03-05 14:41:03 +0100

    Update installbeta.sh

--- src/installbeta.sh
+++ src/installbeta.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export DIRMAIN=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
-echo -e "--------------------"
+echo -e "${GREEN}--------------------"
 echo -e "Justus0405 dotfiles "
 echo -e "Installer Ver. 1.1"
 echo -e "Have a good day!"
-echo -e "--------------------"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 while true; do
@@ -18,9 +21,9 @@ while true; do
 done
 
 echo -e ""
-echo -e "####################"
+echo -e "${GREEN}--------------------"
 echo -e "Installing apps..."
-echo -e "####################"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 sudo pacman -S pipewire lib32-pipewire pipewire-pulse pipewire-jack wireplumber rtkit i3-wm sddm polybar rofi alacritty nitrogen xf86-input-evdev xf86-input-synaptics pcmanfm-gtk3 xarchiver lxappearance-gtk3 neofetch flameshot unzip htop qt5-graphicaleffects qt5-svg qt5-quickcontrols2 wget gvfs gvfs-smb nano code loupe totem gst-libav gst-plugins-ugly gnome-color-manager ffmpeg yt-dlp discord steam obs-studio gimp
@@ -32,9 +35,9 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsM
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.1/Catppuccin-Mocha-Standard-Mauve-Dark.zip -P $DIRMAIN/assets
 
 echo -e ""
-echo -e "--------------------"
+echo -e "${GREEN}--------------------"
 echo -e "Copying files..."
-echo -e "--------------------"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 mkdir -p ~/.config ~/.local/share/fonts ~/.local/share/themes
@@ -52,9 +55,9 @@ sudo mv /usr/share/X11/xorg.conf.d/40-libinput.conf ~/.config/
 sudo cp -r $DIRMAIN/assets/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
 
 echo -e ""
-echo -e "--------------------"
+echo -e "${GREEN}--------------------"
 echo -e "Enabling services..."
-echo -e "--------------------"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 systemctl --user enable pipewire pipewire-pulse
@@ -63,9 +66,9 @@ sudo systemctl disable lightdm
 sudo systemctl enable sddm
 
 echo -e ""
-echo -e "--------------------"
+echo -e "${GREEN}--------------------"
 echo -e "Choosing a Browser"
-echo -e "--------------------"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 PS3='Choose your browser: '
@@ -95,9 +98,9 @@ select fav in "${browsers[@]}"; do
 done
 
 echo -e ""
-echo -e "--------------------"
+echo -e "${GREEN}--------------------"
 echo -e "Finished! Rebooting..."
-echo -e "--------------------"
+echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
 reboot
