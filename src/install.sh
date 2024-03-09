2c1eb26ef8cff727e2cdbdc72058f571a64b728e
Justus0405 <143542687+Justus0405@users.noreply.github.com>
2024-03-10 00:16:35 +0100

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -26,7 +26,7 @@ echo -e "Installing apps..."
 echo -e "--------------------${ENDCOLOR}"
 echo -e ""
 
-sudo pacman -S pipewire lib32-pipewire pipewire-pulse pipewire-jack wireplumber rtkit i3-wm sddm polybar rofi alacritty nitrogen xf86-input-evdev xf86-input-synaptics pcmanfm-gtk3 xarchiver lxappearance-gtk3 neofetch flameshot unzip htop qt5-graphicaleffects qt5-svg qt5-quickcontrols2 wget gvfs gvfs-smb nano code loupe totem gst-libav gst-plugins-ugly gnome-color-manager ffmpeg yt-dlp discord steam obs-studio gimp network-manager-applet papirus-icon-theme ttf-jetbrains-mono-nerd
+sudo pacman -S pipewire lib32-pipewire pipewire-pulse pipewire-jack wireplumber rtkit i3-wm sddm polybar rofi alacritty nitrogen xf86-input-evdev xf86-input-synaptics pcmanfm-gtk3 xarchiver lxappearance-gtk3 neofetch flameshot unzip htop qt5-graphicaleffects qt5-svg qt5-quickcontrols2 wget gvfs gvfs-smb nano code loupe totem gst-libav gst-plugins-ugly gnome-color-manager ffmpeg yt-dlp discord steam obs-studio gimp network-manager-applet gnu-free-fonts papirus-icon-theme ttf-jetbrains-mono-nerd
 
 sudo usermod -a -G rtkit $USER
 sudo localectl set-keymap de
