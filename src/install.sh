50f2b196afc64bfb5b770d13231c80f4598925f1
Justus0405 <143542687+Justus0405@users.noreply.github.com>
2024-06-11 21:33:44 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -183,7 +183,7 @@ echo -e ""
 mkdir -p ~/.config ~/.local/share/themes
 unzip $DIRMAIN/assets/Catppuccin-Mocha-Standard-Mauve-Dark.zip -d ~/.local/share/themes/
 
-echo -e "[xin_-1]\nfile=/home/$USER/.config/wallpaper.jpg\nmode=5\nbgcolor=#000000" > $DIRMAIN/config/nitrogen/bg-saved.cfg
+echo -e "[xin_-1]\nfile=/home/$USER/.config/wallpapers/scenery.png\nmode=5\nbgcolor=#000000" > $DIRMAIN/config/nitrogen/bg-saved.cfg
 chmod +x $DIRMAIN/config/polybar/launch.sh
 cp -r $DIRMAIN/config/* ~/.config/
 
